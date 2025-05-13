d970f484873445a3f45660be5f964027e48d75b8
daftK <du_ikei@hotmail.com>
2025-05-13 07:41:38 -0300

    chore: changed vpc_cidr block to /21 instead of /16 due to MONGODB Atlas

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,17 +2,17 @@
 # with managed node groups
 data "aws_availability_zones" "available" {
   filter {
-    name = "opt-in-status"
+    name   = "opt-in-status"
     values = ["opt-in-not-required"]
   }
 }
 
 locals {
-  name = basename(path.cwd)
-  vpc_cidr             = "10.0.0.0/16"
+  name                    = basename(path.cwd)
+  vpc_cidr                = "10.0.0.0/21"
   availability_zone_names = slice(data.aws_availability_zones.available.names, 0, 3)
-  private_subnet_names = [for k, v in local.availability_zone_names : cidrsubnet(local.vpc_cidr, 8, k)]
-  public_subnet_names  = [for k, v in local.availability_zone_names : cidrsubnet(local.vpc_cidr, 8, k + 4)]
+  private_subnet_names    = [for k, v in local.availability_zone_names : cidrsubnet(local.vpc_cidr, 8, k)]
+  public_subnet_names     = [for k, v in local.availability_zone_names : cidrsubnet(local.vpc_cidr, 8, k + 4)]
   tags = {
     GithubRepo = "fiap-lanchonete-infra"
     GithubOrg  = "fiap-9soat-4"
